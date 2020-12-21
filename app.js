dffb4949f8ba53bec19d055ddaf3687e8c5c0e81
Michael Wideburg <mwideburg@gmail.com>
2020-12-21 01:20:27 -0500

    configure heroku for the push

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const path = require('path');
+
 const db = require('./config/keys_prod').mongoURI;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -7,6 +9,15 @@ const passport = require('passport');
 
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
+
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
+
 
 mongoose
   .connect(db, { useNewUrlParser: true })
